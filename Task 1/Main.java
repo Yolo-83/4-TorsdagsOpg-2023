a54ebf227a3f4fc0624451efd97f88cc8a78f9e9
Yolo-83 <cph-yc123@cphbusiness.dk>
2023-03-03 07:51:49 +0100

    .

---
+++ Task 1/Main.java
@@ -0,0 +1,27 @@
+////  Client class
+
+class Main {
+	// 1a.
+	public static void main(String[] args){
+
+		Team tn = new Team (int "10");
+
+
+	}
+
+}
+
+
+
+
+
+
+
+/*
+In this exercise you will code an entity class Team and a client class Main. 
+You will create private attributes on the entity class 
+and give it a parameterized constructor, a toString and a setter method.
+
+1.a Create a class Main with a main method.
+
+*/
---
+++ Task 1/Team.java
@@ -0,0 +1,66 @@
+////  Entity / Utility class
+
+class Team{
+
+	private String teamName;  // 1c
+	private int teamsRank;  // 1d
+	private String players;  // 1e
+
+
+	Team (String teamName){
+		this.teamsName = teamsName;
+
+	}
+	public void setRank(int rank){
+		this.rank = rank;
+	}
+
+
+}
+
+
+public String setBalance(int amount) {
+	this.balance = amount;	
+	}
+
+
+/*
+
+In this exercise you will code an entity class Team and a client class Main. 
+You will create private attributes on the entity class 
+and give it a parameterized constructor, a toString and a setter method.
+
+1.a Create a class Main with a main method.
+
+1.b Create an entity class called Team.
+
+1.c Add a private instance variable a.k.a attribute to Team to hold the team name.
+
+1.d Add another private instance variable to hold the Teams rank (some number).
+
+1.e Add yet another private instance variable to hold the names of players in the team.
+
+1.f Add a constructor with a parameter, 
+so that the Team can be instantiated with a team name. 
+Make sure the parameter value is assigned to the mathcing instance variable.
+Hint
+public Team(String teamName)
+
+1.g From the main method in the Main class, 
+create an instance of the Team class (instantiate the Team class).
+
+1.h In the Team class add a method called setRank that takes a number 
+and assigns it to the rank attribute.
+
+1.i From the main method, 
+call the method setRank on the instance of Team you created in step 1.g 
+with a number of your choice.
+
+1.j In the Team class add a toString method that returns a String formated like this:
+"Hold: De Uovervindelige Rang: 3"
+(Precise wording will depend on the name 
+and rank of the Team instance you have created in step 1.g)
+
+1.k From the main method, create 5 more Team instances and print them.
+
+*/
